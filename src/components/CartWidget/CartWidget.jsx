0f0e752aa8d99c76e233d4c603fccf8572f996e2
Cele Delgado <89647993+CeleDelgado@users.noreply.github.com>
2023-01-30 22:44:52 -0300

    agregamos los iconos y el componente de itemListContainer

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ export const CartWidget = ({cantCarrito}) => {
     return (
         <div>
             {/* en ves de que diga carrito le agregamos un icono */}
-            <button className="btn btn-light">Carrito</button>
+            <button className="btn btn-light"> <i className="bi bi-cart"/> Carrito</button>
             <p>{cantCarrito}</p>
         </div>
     );
--- src/components/Navbar/Sections/Sections.jsx
+++ src/components/Navbar/Sections/Sections.jsx
@@ -2,15 +2,15 @@ export const Sections = () => {
     return (
         <>
             <li className="nav-item">
-            <a className="nav-link active" href="#">Inicio
+            <a className="nav-link active" href="#"><i className="bi bi-house"/>Inicio
             <span className="visually-hidden">(current)</span>
             </a>
             </li>
             <li className="nav-item">
-            <a className="nav-link" href="#">Acerca de nosotros</a>
+            <a className="nav-link" href="#"><i className="bi bi-person-exclamation"/> Acerca de nosotros</a>
             </li>
             <li className="nav-item">
-            <a className="nav-link" href="#">Contacto</a>
+            <a className="nav-link" href="#"><i className="bi bi-person-workspace"/> Contacto</a>
             </li>
         </>
     );
